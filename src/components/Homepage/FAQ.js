107ef1a43b87b8ec47d1638d210f8df70dc6e966
Anahlira <t.k.slavova@abv.bg>
2024-02-16 16:41:29 +0200

    added questions

--- src/components/Homepage/FAQ.js
+++ src/components/Homepage/FAQ.js
@@ -7,16 +7,44 @@ import { Collapse } from "antd";
 
 const questions = [
   {
-    question: "This is panel header 1",
-    answer: "text 1",
+    question: "What is a DID?",
+    answer:
+      "Decentralised identifiers (DIDs) are a new type of identifier that enable verifiable, decentralised digital identity. A DID refers to any subject (e.g., a person, organisation, thing, data model, abstract entity, etc.) as determined by the controller of the DID. In contrast to typical, federated identifiers, DIDs have been designed so that they may be decoupled from centralised registries, identity providers, and certificate authorities.",
   },
   {
-    question: "This is panel header 2",
-    answer: "text 2",
+    question: "What is a VC?",
+    answer:
+      "Verifiable credentials (VCs) are an open standard for digital credentials. They can represent information found in physical credentials, such as a passport or license, as well as new things that have no physical equivalent, such as ownership of a bank account. They have numerous advantages over physical credentials, most notably that they are digitally signed, which makes them tamper-resistant and instantaneously verifiable.",
   },
   {
-    question: "This is panel header 3",
-    answer: "text 3",
+    question: "How can I get access to EBSI documentation / specifications?",
+    hasLink: true,
+    answer: (
+      <p>
+        EBSI's documentation and specifications are available in the{" "}
+        {
+          <a
+            href="https://hub.ebsi.eu/specs"
+            class="external-link"
+            rel="nofollow"
+            target="_blank"
+          >
+            Specifications section
+          </a>
+        }{" "}
+        and in the{" "}
+        <a
+          href="https://hub.ebsi.eu/"
+          class="external-link"
+          rel="nofollow"
+          target="_blank"
+        >
+          Developers Hub
+        </a>
+        . The specifications are constantly evolving and the documentation will
+        continuously be updated.
+      </p>
+    ),
   },
 ];
 
@@ -26,10 +54,11 @@ export default function FAQ() {
 
   if (!isMounted.current) {
     questions.map((q, index) => {
+      const children = q.hasLink ? q.answer : <p>{q.answer}</p>;
       const item = {
         key: index + 1,
         label: q.question,
-        children: <p>{q.answer}</p>,
+        children: children,
       };
       items.current.push(item);
     });
